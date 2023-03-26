9465cc09a2cde6f03f2bc33c3d727b953f79c1a3
smvjash <sumit.vyas@jashds.com>
2023-03-26 20:01:13 +0530

    generator class created for displaying text

---
+++ generator.py
@@ -0,0 +1,9 @@
+class generator():
+    def __init__(self):
+        pass
+    def echo_text(self, text):
+        print(text)
+
+if __name__ == '__main__':
+    text_generator = generator()
+    text_generator.echo_text("This is a demo text")
