f80d5af18e8586ef416c6f0edabc498e341a865b
smvjash <sumit.vyas@jashds.com>
2023-03-26 20:17:32 +0530

    echo_text function modified to print list

--- generator.py
+++ generator.py
@@ -2,9 +2,8 @@ class generator():
     def __init__(self):
         self.text_ls = []
 
-    def echo_text(self):
-        for text in self.text_ls:
-            print(text)
+    def echo_text(self, text):
+        print(text)
             
     def add_text(self, text):
         self.text_ls.append(text)
